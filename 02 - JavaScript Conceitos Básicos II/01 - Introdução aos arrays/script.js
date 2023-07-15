6e2d25a32c027ed2d46539e926796850db6e7f21
Matheus Urban <matheusurban19@gmail.com>
2023-07-14 23:07:38 -0300

    Arrays

--- 02 - JavaScript Conceitos Básicos II/01 - Introdução aos arrays/script.js
+++ 02 - JavaScript Conceitos Básicos II/01 - Introdução aos arrays/script.js
@@ -1,1 +1,25 @@
 //Introdução aos arrays
+var familia = ['Matheus', 'Giulia', 'Ismael', 'Timóteo', 'Amanda', 'Bianca']
+
+var alunos = Array('Matheus', 'Giulia', 'Ismael', 'Timóteo', 'Amanda', 'Bianca')
+
+var cliente = {
+    nome: 'Nome: Matheus Urban',
+    idade:'Idade: ' + 21,
+    peso: 'Peso: ' + 98,
+    altura: 'Altura: ' + 1.75,
+    cpf: 'CPF: ' + 51906993823
+}
+
+console.log(cliente.nome);
+console.log(cliente.idade, cliente.peso);
+console.log(cliente.altura, cliente.cpf);
+
+
+console.log(familia);
+console.log(alunos);
+
+/* Detalhes sobre arrays
+- Possuem tamanho infinito
+- Podemos colocar qualquer tipo de dado
+*/
