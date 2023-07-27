01b24e546a95fdaf57a669c7a6290a8b2ac82b76
Matheus Urban <matheusurban19@gmail.com>
2023-07-27 20:44:17 -0300

    Aprendendo JS

--- 03 - Funções/01 - First Class Citizens/script.js
+++ 03 - Funções/01 - First Class Citizens/script.js
@@ -3,3 +3,22 @@
 function somar(n1, n2){
     return n1 + n2;
 }
+
+//Exemplo 1
+let res = somar(5,6);
+console.log(res);
+
+//Exemplo 2
+const exec = somar;
+console.log(exec(8, 7));
+
+function sub(n1, n2){
+    return n1 - n2;
+}
+
+function fazAlgo(n1, n2, funcao){
+    return funcao(n1, n2);
+}
+
+console.log(fazAlgo(5, 5, somar));
+console.log(fazAlgo(5, 5, sub));
