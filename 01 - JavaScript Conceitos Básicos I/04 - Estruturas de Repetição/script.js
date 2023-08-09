fa57b0dee13e789308eeae5193586c736d3d5aff
Matheus Urban <matheusurban19@gmail.com>
2023-08-09 02:21:16 -0300

    while

--- 01 - JavaScript Conceitos Básicos I/04 - Estruturas de Repetição/script.js
+++ 01 - JavaScript Conceitos Básicos I/04 - Estruturas de Repetição/script.js
@@ -15,7 +15,37 @@
         //do-while
         var cont = 0;
 
-        do{
-            console.log('Imprime o valor de cont: ' + cont);
-            cont++
-        }while(cont <= 5)
+        //do{
+        //    console.log('Imprime o valor de cont: ' + cont);
+        //    cont++
+        //}while(cont <= 5)
+
+        //Testes
+
+        let animais = [
+            'Tartaruga',
+            'Baleia',
+            'Tubarão',
+            'Golfinho',
+            'Baiacu',
+            'Arraia',
+            'Anemona',
+            'Cavalo-Marinho',
+            'Orca',
+            'Peixe-Boi'
+        ];
+
+        let contador = 0;
+
+        while(contador < animais.length){
+            console.log(animais);
+            contador++
+        }
+
+
+        let numero = 0;
+
+        while(numero <= 10){
+            console.log(numero);
+            numero++
+        }
