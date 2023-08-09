118f11890a24f1138b05ade67277f91469d1702d
Matheus Urban <matheusurban19@gmail.com>
2023-08-09 14:40:32 -0300

    funções

---
+++ 02 - JavaScript Conceitos Básicos II/11 - Resumindo coisas importantes/script.js
@@ -0,0 +1,13 @@
+//Anotações e Resumindo algumas coisas
+
+let numero = [1, 5, 6,]
+
+numero.map // cria um array com o msm lenght com alterações ditas por uma function.
+
+numero.lenght // comprimento do array.
+
+numero.reduce // reduz o array usando em base o pedido de uma function.
+
+numero.forEach // significa para cada, ou seja, para cada informaçção da array, ocorre uma função, usando em base (óbvio) function.
+
+numero.filter // filtra os dados de uma variável usando uma function como parâmetro.
--- 03 - Funções/05 - Funções Anônimas (Lambdas)/script.js
+++ 03 - Funções/05 - Funções Anônimas (Lambdas)/script.js
@@ -1,1 +1,26 @@
 //Funções Anônimas (Lambdas)
+
+let numeros = [1, 2, 3, 4, 5, 6]
+
+function dobrar(valor){
+    return valor * 2
+}
+
+let res1 = numeros.map(dobrar)
+
+console.log(numeros);
+console.log(res1);
+
+//função lambda
+
+let re2 = numeros.map(function(valor){
+    return valor * 2
+}) // a gente passa a function como parâmetro pro map
+
+console.log(re2);
+
+let res3 = function(valor){
+    return valor * 2
+}
+
+console.log(res3(9));
