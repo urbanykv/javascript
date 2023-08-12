33252c8a159191c4995a3bdb1137c15480fa20b7
Matheus Urban <matheusurban19@gmail.com>
2023-08-11 21:09:00 -0300

    functions

--- 03 - Funções/06 - Funções Callbacks/script.js
+++ 03 - Funções/06 - Funções Callbacks/script.js
@@ -18,3 +18,18 @@ function apresentar(curso, i){
 }
 
 cursos.forEach(apresentar)
+
+let alunos = [
+    {id: 1, nome: 'Laura', media: 4},
+    {id: 2, nome: 'Pedro', media: 3},
+    {id: 3, nome: 'Samuel', media: 5},
+    {id: 4, nome: 'Samantha', media: 7},
+    {id: 5, nome: 'Matheus', media: 8},
+    {id: 6, nome: 'Giulia', media: 10}
+];
+
+function apresentando(aluno, i){
+    console.log(`${i + 1} - a média final do aluno: ${aluno.nome} de id: ${aluno.id} é ${aluno.media}`);
+}
+
+alunos.forEach(apresentando)
--- 03 - Funções/07 - Funções Construtoras/script.js
+++ 03 - Funções/07 - Funções Construtoras/script.js
@@ -1,1 +1,22 @@
-//Funções Construtoras
+let disciplinas = [
+    { id: 1, nome: "português", carga_horaria: 250 },
+    { id: 2, nome: "matemática", carga_horaria: 220 },
+    { id: 3, nome: "história", carga_horaria: 150 },
+    { id: 4, nome: "geografia", carga_horaria: 150 },
+    { id: 5, nome: "inglês", carga_horaria: 70 }
+];
+
+
+for(let disciplina of disciplinas){
+
+    let id_disciplina = disciplina.id;
+    let nome_disciplina = disciplina.nome;
+    let carga_horaria_disciplina = disciplina.carga_horaria;
+
+    console.log("Id da disciplina: " + id_disciplina);
+    console.log("Nome da disciplina: " + nome_disciplina);
+    console.log("Carga horária da disciplina: " + carga_horaria_disciplina);
+
+    console.log("\n");
+
+}
