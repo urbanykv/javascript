630a96079f21b22ff417550a729c8de05ac5d9d6
urbanykv <matheusurban19@gmail.com>
2023-09-29 11:18:15 -0300

    Async Await

--- 12 - Testes e Exercícios/ex007/script.js
+++ 12 - Testes e Exercícios/ex007/script.js
@@ -1,33 +1,24 @@
 let num = 150
 
-const promise = new Promise((resolve, reject) => {
+const promise = () => new Promise((resolve, reject) => {
     const conta2 = num + 300
 
-    if(conta2 !== 500){
-        resolve("Incorreto!!");;
-    }else{
-        reject("Correto!!");
-    }
-})
+    setInterval(() => {
+        if(conta2 === 450){
+            resolve("Correto!!");
+        }else{
+            reject("Incorreto!!");
+        }
+    }, 2000)
+});
 
-.then(messagemResolve => {
-    console.log(messagemResolve);
-}) .catch(mensagemReject => { 
-    console.log(mensagemReject);
-})
-
-
-
-const primeiraFunction = () => {
-    const conta = num + 200
-
-    if(conta === 350){
-        console.log("Correto!!");
-    }else{
-        console.log("Incorreto!!");
+async function executarPromise(){
+    try{
+        const response = await promise()
+        console.log(response);
+    }catch(erro){
+        console.log(erro);
     }
 }
 
-
-
-primeiraFunction()
+executarPromise()
